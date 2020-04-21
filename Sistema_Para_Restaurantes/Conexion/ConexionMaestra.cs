7a8352b3763e2cbda5977b02d802dbb1debb26d5
WizJuanRD <juan_r.d12@hotmail.com>
2020-04-21 16:37:06 -0400

    Inicio del proyecto

---
+++ Sistema_Para_Restaurantes/Conexion/ConexionMaestra.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Para_Restaurantes.Conexion
+{
+    class ConexionMaestra
+    {
+        public static string conexion = Convert.ToString(Librerias.Desencryptacion.checkServer());
+    }
+}
---
+++ Sistema_Para_Restaurantes/Librerias/Desencryptacion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Sistema_Para_Restaurantes.Librerias
+{
+    class Desencryptacion
+    {
+        static private AES aes = new AES();
+        static public string CnString;
+        static string dbcnString;
+        static public string appPwdUnique = "RESTAURANTE.wizjuanrd.BASEREST.Hola_MundoÑ";
+
+
+        public static object checkServer()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("ConnectionString.xml");
+            XmlElement root = doc.DocumentElement;
+            dbcnString = root.Attributes[0].Value;
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+
+        internal class label
+        {
+
+        }
+        public static object UsuariosEncryp()
+        {
+            XmlDocument doc = new XmlDocument();
+            label root = new label();
+
+            dbcnString = root.ToString();
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+        }
+    }
+}
